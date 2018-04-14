b0520625ebe36b3d3a4da7dbe902a304c9a40283
Nikita Chernyshov <xpom55@mail.ru>
2018-04-14 13:20:20 +0300

    add checking for leaving the main screen by X size

--- main.py
+++ main.py
@@ -13,42 +13,55 @@ blue = (0, 0, 255)
 
 headImg = pygame.image.load('car.png')
 
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+
 def head(x, y):
   gameDisplay.blit(headImg, (x, y))
 
-x = (display_width * 0.45)
-y = (display_height * 0.8)
+def check(x):
+  if (x > display_width or x < 0):
+    return True
+  return False
 
-gameDisplay = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption('A bit Racey')
-clock = pygame.time.Clock()
 
-crashed = False
-x_change = 0
-while not crashed:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      crashed = True
+def game_loop():
+  x = (display_width * 0.45)
+  y = (display_height * 0.8)
 
-    if event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_LEFT:
-        x_change = -5;
-      if event.key == pygame.K_RIGHT:
-        x_change = +5;
+  pygame.display.set_caption('A bit Racey')
+  clock = pygame.time.Clock()
 
-    if event.type == pygame.KEYUP:
-      if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-        x_change = 0;
+  gameExit = False
+  x_change = 0
 
-    print(event)
+  while not gameExit:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        gameExit = True
 
-  x += x_change
-  gameDisplay.fill(white)
-  head(x, y)
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+          x_change = -5;
+        if event.key == pygame.K_RIGHT:
+          x_change = +5;
 
-  pygame.display.update()
-  clock.tick(60)
+      if event.type == pygame.KEYUP:
+        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+          x_change = 0;
 
+      print(event)
+
+    x += x_change
+
+    gameExit = check(x) or gameExit
+
+    gameDisplay.fill(white)
+    head(x, y)
+
+    pygame.display.update()
+    clock.tick(60)
+
+game_loop()
 pygame.quit()
 quit()
 
