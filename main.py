19d39a1d00e8a93551386c18648f74d5f13c9095
Nikita Chernyshov <xpom55@mail.ru>
2018-04-14 13:10:26 +0300

    command pythonm main.py for run it properly, added png img and it can be moved left or right =)

--- main.py
+++ main.py
@@ -1,17 +1,50 @@
 import pygame
 
 pygame.init()
-gameDisplay = pygame.display.set_mode((800, 600))
+
+display_width = 800
+display_height = 600
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+headImg = pygame.image.load('car.png')
+
+def head(x, y):
+  gameDisplay.blit(headImg, (x, y))
+
+x = (display_width * 0.45)
+y = (display_height * 0.8)
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('A bit Racey')
 clock = pygame.time.Clock()
 
 crashed = False
+x_change = 0
 while not crashed:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       crashed = True
 
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_LEFT:
+        x_change = -5;
+      if event.key == pygame.K_RIGHT:
+        x_change = +5;
+
+    if event.type == pygame.KEYUP:
+      if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+        x_change = 0;
+
     print(event)
+
+  x += x_change
+  gameDisplay.fill(white)
+  head(x, y)
 
   pygame.display.update()
   clock.tick(60)
