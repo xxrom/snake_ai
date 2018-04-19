8d21174b6d1cd7c60595a19239c528199644a051
Chernyshov <xpom55@mail.ru>
2018-04-19 16:17:12 +0300

    distance to the food, direction marker and refactoring

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import math
 
 pygame.init()
 
-WIDTH = 200
-HEIGHT = 200
-FPS = 20
+FPS = 10
 IN_GAME_FPS = 1000 // FPS;
 
-cellWidth = 20
-cellHeight = 20
+SNAKE_EAT_AREA = 0 # расстояние в клеточках за которое будет кушать змея
+
+CELL_WIDTH = 20
+CELL_HEIGHT = 20
+
+WIDTH = CELL_WIDTH * 20
+HEIGHT = CELL_HEIGHT * 20
 
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -37,59 +40,75 @@ food = {
   'y': 0
 }
 
-headImg = pygame.image.load('car.png')
+# headImg = pygame.image.load('car.png')
 
 gameDisplay = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
 def init_food():
   print(random.random())
-  food['x'] = math.floor(random.random() * (WIDTH / cellWidth)) * cellWidth
-  food['y'] = math.floor(random.random() * (HEIGHT / cellHeight)) * cellHeight
+  food['x'] = math.floor(random.random() * (WIDTH / CELL_WIDTH)) * CELL_WIDTH
+  food['y'] = math.floor(random.random() * (HEIGHT / CELL_HEIGHT)) * CELL_HEIGHT
   print(food)
 
-def draw_head(x, y):
-  pygame.draw.rect(gameDisplay, (0, 200, 10), (x, y, cellWidth, cellHeight))
-  # pygame.draw.circle(gameDisplay, (0, 200, 0), (x + (cellWidth // 2), y + (cellHeight // 2)), cellHeight // 2)
+def init_snake(snakeObj):
+  snakeObj['x'] = (CELL_WIDTH * 1)
+  snakeObj['y'] = (CELL_HEIGHT * 1)
+  snakeObj['length'] = 0
+  snakeObj['tail'] = []
+  snakeObj['direction'] = 'down'
+
+def draw_head(snake):
+  pygame.draw.rect(gameDisplay, (0, 200, 10), (snake['x'], snake['y'], CELL_WIDTH, CELL_HEIGHT))
+
+  if (snake['direction'] == 'down'):
+    pygame.draw.circle(gameDisplay, (200, 0, 0), (snake['x'] + CELL_WIDTH // 2, snake['y'] + CELL_HEIGHT), CELL_HEIGHT // 5)
+  if (snake['direction'] == 'right'):
+    pygame.draw.circle(gameDisplay, (200, 0, 0), (snake['x'] + CELL_WIDTH, snake['y'] + CELL_HEIGHT // 2), CELL_HEIGHT // 5)
+  if (snake['direction'] == 'up'):
+    pygame.draw.circle(gameDisplay, (200, 0, 0), (snake['x'] + CELL_WIDTH // 2, snake['y']), CELL_HEIGHT // 5)
+  if (snake['direction'] == 'left'):
+    pygame.draw.circle(gameDisplay, (200, 0, 0), (snake['x'], snake['y'] + CELL_HEIGHT // 2), CELL_HEIGHT // 5)
+
+  # pygame.draw.circle(gameDisplay, (0, 200, 0), (x + (CELL_WIDTH // 2), y + (CELL_HEIGHT // 2)), CELL_HEIGHT // 2)
   # gameDisplay.blit(headImg, (x, y))
 
 def draw_tail(tail, length):
   for i in range(0, length):
-    pygame.draw.rect(gameDisplay, (0, 255, 0), (tail[i]['x'], tail[i]['y'], cellWidth, cellHeight))
+    pygame.draw.rect(gameDisplay, (0, 255, 0), (tail[i]['x'], tail[i]['y'], CELL_WIDTH, CELL_HEIGHT))
 
 def draw_food(x, y):
-  pygame.draw.circle(gameDisplay, red, (x + (cellWidth // 2), y + (cellHeight // 2)), 10)
-
+  pygame.draw.circle(gameDisplay, red, (x + (CELL_WIDTH // 2), y + (CELL_HEIGHT // 2)), 10)
 
 def check(x, y):
   if (
-    x > WIDTH - cellWidth
+    x > WIDTH - CELL_WIDTH
     or x < 0
     or y < 0
-    or y > HEIGHT - cellHeight
+    or y > HEIGHT - CELL_HEIGHT
   ):
     return True
-  return False
+  return False # вышли за границу поля
 
 def textObjects(text, font, color):
   textSurface = font.render(text, True, color)
   return textSurface, textSurface.get_rect()
 
-def messageDisplay(text):
-  largeText = pygame.font.Font('freesansbold.ttf', 80)
+def messageDisplay(text, textSize = 80, x = (WIDTH / 2), y = (HEIGHT / 2)):
+  largeText = pygame.font.Font('freesansbold.ttf', textSize)
   TextSurf, TextRect = textObjects(text, largeText, black)
-  TextRect.center = ((WIDTH / 2), (HEIGHT / 2))
+  TextRect.center = (x, y)
   gameDisplay.blit(TextSurf, TextRect)
 
 def crash():
   print('You Crashed!')
-  messageDisplay('You Crashed!')
+  messageDisplay('You Crashed!', 50)
 
 def draw_field():
-  for i in range(0, WIDTH // cellWidth):
-    for j in range(0, HEIGHT // cellHeight):
+  for i in range(0, WIDTH // CELL_WIDTH):
+    for j in range(0, HEIGHT // CELL_HEIGHT):
       pygame.draw.rect(gameDisplay, (230,230,230),
-        (i * cellWidth, j * cellHeight, cellWidth, cellHeight), 1)
+        (i * CELL_WIDTH, j * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT), 1)
 
 def millis():
     return int(round(time.time() * 1000))
@@ -103,13 +122,13 @@ def events():
     keys = pygame.key.get_pressed()
     if event.type == pygame.KEYDOWN:
       if keys[pygame.K_LEFT]:
-        pressedKeys['left'] = -cellWidth
+        pressedKeys['left'] = -CELL_WIDTH
       if keys[pygame.K_RIGHT]:
-        pressedKeys['right'] = cellWidth
+        pressedKeys['right'] = CELL_WIDTH
       if keys[pygame.K_UP]:
-        pressedKeys['up'] = -cellHeight
+        pressedKeys['up'] = -CELL_HEIGHT
       if keys[pygame.K_DOWN]:
-        pressedKeys['down'] = cellHeight
+        pressedKeys['down'] = CELL_HEIGHT
 
     if event.type == pygame.KEYUP:
       if keys[pygame.K_LEFT] == 0:
@@ -123,13 +142,50 @@ def events():
 
     print(event)
 
+def snake_moves():
+  if (snake['direction'] == 'down'):
+    if (pressedKeys['left'] != 0):
+      snake['direction'] = 'left'
+    if (pressedKeys['right'] != 0):
+      snake['direction'] = 'right'
+
+  if (snake['direction'] == 'right'):
+    if (pressedKeys['up'] != 0):
+      snake['direction'] = 'up'
+    if (pressedKeys['down'] != 0):
+      snake['direction'] = 'down'
+
+  if (snake['direction'] == 'up'):
+    if (pressedKeys['left'] != 0):
+      snake['direction'] = 'left'
+    if (pressedKeys['right'] != 0):
+      snake['direction'] = 'right'
+
+  if (snake['direction'] == 'left'):
+    if (pressedKeys['up'] != 0):
+      snake['direction'] = 'up'
+    if (pressedKeys['down'] != 0):
+      snake['direction'] = 'down'
+
+  if (snake['direction'] == 'down'):
+    snake['y'] += CELL_HEIGHT
+  if (snake['direction'] == 'up'):
+    snake['y'] -= CELL_HEIGHT
+  if (snake['direction'] == 'left'):
+    snake['x'] -= CELL_WIDTH
+  if (snake['direction'] == 'right'):
+    snake['x'] += CELL_WIDTH
+
+  # snake['x'] += pressedKeys['left'] + pressedKeys['right']
+  # snake['y'] += pressedKeys['up'] + pressedKeys['down']
+
+def draw_info(snake, food):
+  distance = math.sqrt((snake['x'] - food['x'])**2 + (snake['y'] - food['y'])**2) # расстояние в пикселях
+  messageDisplay(str(int(distance)), 20, CELL_WIDTH, CELL_HEIGHT)
+
 def game_loop():
   print('game loop')
-  # time.sleep(1)
-  snake['x'] = (cellWidth * 1)
-  snake['y'] = (cellHeight * 1)
-  snake['length'] = 0
-  snake['tail'] = []
+  init_snake(snake)
 
   pressedKeys['left'] = 0
   pressedKeys['right'] = 0
@@ -156,8 +212,8 @@ def game_loop():
 
     prev_x = snake['x']
     prev_y = snake['y']
-    snake['x'] += pressedKeys['left'] + pressedKeys['right']
-    snake['y'] += pressedKeys['up'] + pressedKeys['down']
+
+    snake_moves()
 
     # передвигаем весь хвост
     if (snake['x'] != prev_x or snake['y'] != prev_y):
@@ -167,7 +223,7 @@ def game_loop():
     checkCrash = check(snake['x'], snake['y'])
 
     if checkCrash == False:
-      eatArea = cellWidth * 3
+      eatArea = CELL_WIDTH * SNAKE_EAT_AREA
       if (abs(snake['x'] - food['x']) <= eatArea
         and abs(snake['y'] - food['y']) <= eatArea):
         print(snake['tail'])
@@ -189,7 +245,9 @@ def game_loop():
     draw_field()
     draw_tail(snake['tail'], snake['length'])
     draw_food(food['x'], food['y'])
-    draw_head(snake['x'], snake['y'])
+    draw_head(snake)
+
+    draw_info(snake, food)
 
     if checkCrash == True:
       startTime = millis() + 1000
